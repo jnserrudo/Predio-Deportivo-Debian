2a97f404d35174a4a69de6a3d69b4a62c3f1d8d1
nahuel <jnserrudo@gmail.com>
2021-11-16 01:20:56 -0300

    avance31 ultimo commit

--- php/insercion_remito.php
+++ php/insercion_remito.php
@@ -4,20 +4,34 @@
 
     $conexion = NULL;
         try{
+            $conexion = mysqli_connect('localhost','root','','debian2');
 
-         
+            $sql_registe = mysqli_query($conexion,"SELECT COUNT(*) as total_registro FROM insumo");
+			$result_register = mysqli_fetch_array($sql_registe);
+			$total_registro = $result_register['total_registro'];
+
+			$por_pagina = 5;
+            if(empty($_GET['p']))
+			{
+				$pagina = 1;
+			}else{
+				$pagina = $_GET['p'];
+			}
+
+			$desde = ($pagina-1) * $por_pagina;         
 
          
             if (isset($_GET['x'])) {
                 $c = $_GET['x'];
-
+                
+               
                 $sql = "SELECT * FROM insumo where Id like '%$c%' or Id_categoria like '%$c%' or Nombre like '%$c%'
-                or Descripcion like '%$c%' 
+                or Descripcion like '%$c%'LIMIT $desde,$por_pagina 
 ";
 
             }
             else{
-                $sql = "SELECT * FROM insumo ";
+                $sql = "SELECT * FROM insumo LIMIT $desde,$por_pagina ";
             }
 
 
